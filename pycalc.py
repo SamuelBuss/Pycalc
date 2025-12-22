0b3f9f6b443bd14ad70eebc239106648a9bf7608
Samuel Buss <samuelbuss@seznam.cz>
2025-12-22 20:50:40 +0100

    First working version taking basic input

--- pycalc.py
+++ pycalc.py
@@ -13,4 +13,22 @@ def division(a, b):
     return a / b
 equation = input("Enter your equation: ")
 equation = equation.split()
-print(equation[0])
+if len(equation) != 3:
+    print("Error: Please enter an equation in the format: number operator number")
+    exit()
+a = equation[0]
+operator = equation[1]
+b = equation[2]
+if operator not in ['+', '-', '*', '/']:
+    print("Error: Unsupported operator. Please use one of +, -, *, /.")
+    exit()
+if operator == '+':
+    result = addition(float(a), float(b))
+elif operator == '-':
+    result = subtraction(float(a), float(b))
+elif operator == '*':
+    result = multiplication(float(a), float(b))
+elif operator == '/':
+    result = division(float(a), float(b))
+print("Result:", result)
+exit()
